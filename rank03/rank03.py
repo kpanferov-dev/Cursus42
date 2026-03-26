aa559ec677a75195f1562003fe6e8ae962726d2d
Kirill <kirilljiayou93@gmail.com>
2026-03-26 23:11:10 +0100

    exam

--- rank03/rank03.py
+++ rank03/rank03.py
@@ -1,3 +1,6 @@
+"""Rank03"""
+
+
 def isValid(s: str) -> bool:
     stack = []
     mapping = {')': '(',
@@ -110,3 +113,62 @@ def twoSum(nums, target):
         if diff in seen:
             return [seen[diff], i]
         seen[n] = i
+
+
+"""
+My exam
+"""
+# 1 order list by len, alph, num of vowels
+
+
+def cryptic_sorter(strings: list[str]) -> list[str]:
+    def count_vowels(string: str) -> int:
+        vowels = "aeiouAEIOU"
+        return sum(1 for c in string if c in vowels)
+    return sorted(strings,
+                  key=lambda x: (len(x), x.lower(), count_vowels(x)))
+
+
+# 2 reverse rows of matrix
+def mirror_matrix(matrix: list[list[int]]) -> list[list[int]]:
+    aux = []
+    for row in matrix:
+        aux.append(row[::-1])
+    return aux
+
+
+# 3 number consecutive digits with only
+# 1 of difference between the second and first
+def pattern_tracker(text: str) -> int:
+    count = 0
+    for c in range(len(text) - 1):
+        if (text[c].isdigit() and text[c + 1].isdigit()
+           and int(text[c + 1]) - int(text[c]) == 1):
+            count += 1
+    return count
+
+
+# 4 Anagram
+def string_permutation_checker(s1: str, s2: str) -> bool:
+    return sorted(s1) == sorted(s2)
+
+
+# 5 Move las k elems to start of the list
+def twist_sequence(arr: list[int], k: int) -> list[int]:
+    n = len(arr)
+    if n == 0:
+        return []
+    k %= n
+    return (arr[-k::] + arr[:-k:])
+
+
+# 6 Cesar cipher, char + shift to get a new letter
+def whisper_cipher(text: str, shift: int) -> str:
+    result = ""
+    for c in text:
+        if c.isalpha():
+            base = ord('a') if c.islower() else ord('A')
+            result += chr((ord(c) - base + shift) % 26 + base)
+        else:
+            result += c
+    return result
